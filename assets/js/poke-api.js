f2cabfbd83389d3baec8bcbcee17bf0189482174
matheeusmeloo93 <rmelomatheus@gmail.com>
2023-08-30 00:20:31 -0300

    Colocando função no button para ass mais pokes

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -18,8 +18,8 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-    .then((response) => response.json())
-    .then(convertPokeApiDetailToPokemon)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
     pokeApi.getPokemons = (offset = 0, limit = 5) => {
